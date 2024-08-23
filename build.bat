74d2e99ffe9c2e3b4e736eb0d3d4949df100db3a
Jerry Zhu <jerry.zhu@stonybrook.edu>
2024-08-22 22:59:35 -0400

    fix build script

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ config.cpp controller_asset_set.cpp input_layout_visual.cpp main.cpp -o inputoverlay -std=gnu++11 -lmingw32 -lcomdlg32 -lSDL2main -lSDL2 -lSDL2_image -mwindows -O2
+g++ src/config.cpp src/controller_asset_set.cpp src/input_layout_visual.cpp src/main.cpp -o inputoverlay -std=gnu++11 -lmingw32 -lcomdlg32 -lSDL2main -lSDL2 -lSDL2_image -mwindows -O2
