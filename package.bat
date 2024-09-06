315a82d5c280aafc9d6c3b2c45e589016dad37a3
Jerry Zhu <xpost2000cod@gmail.com>
2024-09-06 12:04:00 -0400

    fix build script + add mouse assets

--- package.bat
+++ package.bat
@@ -1,1 +1,1 @@
-zip -r inputoverlay-release-v0.zip Controllers/ src/ build.bat inputoverlay.exe package.bat SDL2.dll SDL2_image.dll
+zip -r inputoverlay-release-v0.zip Controllers/ src/ build.bat inputoverlay.exe package.bat SDL2.dll SDL2_image.dll libgcc_s_seh-1.dll libstdc++-6.dll
