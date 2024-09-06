8f06124447d6ce2369addb656b83b6bb530b71a4
Jerry Zhu <xpost2000cod@gmail.com>
2024-09-06 12:30:53 -0400

    mouse_asset_set layout fix

--- src/mouse_asset_set.cpp
+++ src/mouse_asset_set.cpp
@@ -2,11 +2,13 @@
 #include "mouse_asset_set.h"
 
 const char* mouse_asset_image_list[MOUSE_ASSET_SET_COUNT][MOUSE_ASSET_COUNT] = {
-    "Controllers/Mouse/mousebase.png",
-    "Controllers/Mouse/mousefill.png",
-    "Controllers/Mouse/left.png",
-    "Controllers/Mouse/middle.png",
-    "Controllers/Mouse/right.png",
+    { // Base
+        "Controllers/Mouse/mousebase.png",
+        "Controllers/Mouse/mousefill.png",
+        "Controllers/Mouse/left.png",
+        "Controllers/Mouse/middle.png",
+        "Controllers/Mouse/right.png",
+    }
 };
 
 const char* mouse_asset_set_strings[MOUSE_ASSET_SET_COUNT] = {
